a9f2ce01023c27b76972dd05e1d82df38a796c5e
Екатерина <165673595+RinHideki@users.noreply.github.com>
2024-10-23 20:08:41 +0500

    алканы.py

--- алканы.py
+++ алканы.py
@@ -1,4 +1,19 @@
-carbon = int(input('Введите количество атомов углерода для рассчёта формулы алкана'))
+carbon = int(input('Введите количество атомов углерода для рассчёта формулы алкана(до 10 включительно'))
 hydrogen = 2 * carbon + 2
 formula = f'C{carbon}H{hydrogen}'
-print(formula)
+alkans = {
+    'C1H4' : 'метан',
+    'C2H6' : 'этан',
+    'C3H8' : 'пропан',
+    'C4H10' : 'бутан',
+    'C5H12' : 'пентан',
+    'C6H14' : 'гексан',
+    'C7H16' : 'гептан',
+    'C8H18' : 'октан',
+    'C9H20' : 'нонан',
+    'C10H22' : 'декан'
+}
+for a in alkans:
+    if a == formula:
+        x = alkans.get(a)
+        print(f'{formula} - это {x}')
